d2e42d6d694723faf2b895285110c24e6dcaad14
Diego Maroto <diego.diemg@gmail.com>
2022-04-08 19:59:23 +0200

    Added dotenv to read env files

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
-
 const app = express();
 app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
